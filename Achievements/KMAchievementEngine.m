32eb607142a7aac5480af9bd0ebc135ea61cd2b8
Michael Tindal <babyplatypus@me.com>
2010-01-10 23:17:42 +0300

    Register the coordinator object with the achievement before checking its criteria.

--- Achievements/KMAchievementEngine.m
+++ Achievements/KMAchievementEngine.m
@@ -30,6 +30,7 @@
 	for(NSString* category in [achievements allKeys]) {
 		for(KMAchievement* a in [achievements objectForKey:category]) {
 			XSHNode* c = [a earnCriteria];
+			[[c scope] registerVariable:coordinator withName:@"coordinator"];
 			BOOL earn =  [[c execute] boolValue];
 			if(earn) {
 				[a displayAchievementHasBeenEarnedMessageTo:coordinator];
